db7f02f956b02ef6aa0b94ec77cd5f21cfe29c43
gxx <1152862327@qq.com>
2021-07-09 13:51:48 +0800

    添加营销机机会-前端弹出层

--- src/main/resources/public/js/saleChance/add.update.js
+++ src/main/resources/public/js/saleChance/add.update.js
@@ -4,6 +4,42 @@ layui.use(['form', 'layer'], function () {
         $ = layui.jquery;
 
     /**
+     * 监听表单submit事件
+     *  form.on('submit(按钮元素的lay-filter属性值)', function (data) {
+     *  });
+     */
+    form.on('submit(addOrUpdateSaleChance)', function (data) {
+        // 提交数据时的加载层
+        var index = layer.msg("数据提交中，请稍后...", {
+            icon: 16,   // 图标
+            time: false,    // 不关闭
+            shade: 0.8  // 设置遮罩的透明度
+        });
+
+        // 发送ajax请求
+        var url = ctx + "/sale_chance/add"; // 添加操作
+        $.post(url, data.field, function (result) {
+            // 判断操作是否执行成功 200 = 成功
+            if (result.code == 200) {
+                // 成功
+                // 提示成功
+                layer.msg("操作成功", {icon: 6});
+                // 关闭加载层
+                layer.close(index)
+                // 关闭弹出层
+                layer.closeAll("iframe");
+                // 刷新父窗口，重新加载数据
+                parent.location.reload();
+            } else {
+                // 失败
+                layer.msg(result.msg, {icon: 5});
+            }
+        });
+        // 阻止表单跳转。如果需要表单跳转，去掉这段即可
+        return false;
+    });
+
+    /**
      * 关闭弹出层
      */
     $("#closeBtn").click(function () {
